952f2691af6b1d76a8013fd6c5369ad76893f861
Ilnura <120139580+ilnura-it@users.noreply.github.com>
2023-02-08 20:23:46 -0700

    Create First_shell.sh

---
+++ First_shell.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+#var=foo
+
+#echo $var
+
+#echo ${var}
+
+#var=foo
+
+#echo $varbar
+
+#echo ${var}bar
+
+#echo ${var} + bar
+
+#echo ${var}{var}bar+bar
+
+#!/usr/bin/env bash
+
+#var=foo
+
+#echo $var
+
+#echo ${var}
+
+var=foo
+
+echo $varbar
+
+echo ${var}bar
+
+echo ${var} + bar
+
+echo ${var}{var}bar+bar
