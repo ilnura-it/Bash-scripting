eea893bc6d400d7327f47ab16348dc6ce312debc
Ilnura <120139580+ilnura-it@users.noreply.github.com>
2023-02-11 10:41:05 -0700

    Update for_loop.sh

--- for_loop.sh
+++ for_loop.sh
@@ -4,3 +4,24 @@ for name in `cat students.txt`;
 do
    echo "My name is $name"
 done
+
+
+FRUIT=("apple" "peach" "banana" "orange");
+
+for fruit in ${FRUIT[@]}
+do
+    echo "This is a $fruit fruit"
+done
+
+#Install packages on redhat servers
+
+SERVERS=("server1" "server2" "server3")
+
+for i in ${SERVERS[@]}
+do
+    echo "Updating packages on $i"
+    sudo apt update
+    echo "============"
+    echo "Installing vim packages in $i"
+    sudo apt install vim
+done
